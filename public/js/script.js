d64f6a7e4cb38c3a4bbc9bd1f6fd405014f845d5
Keith Kovach <kovachks90@gmail.com>
2018-01-24 11:15:21 -0500

    working prototype

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // Setting submit click handler in order to send data to the Mailchimp API
+    $("#sendApi").click(function() {
+
+        // preventing button default event
+        event.preventDefault()
+
+        //Setting info object to be passed to API
+        let info = {
+            "email_address": $("#email").val(),
+            "status": "subscribed",
+            "merge_fields":{
+                "FNAME": $("#firstName").val(),
+                "LNAME": $("#lastName").val()
+            }
+        }
+        
+        console.log(info)
+
+        //Initiating get route to send info to API
+        $.get("/send", info, function(data) { 
+        })
+    })    
+})
+
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var app = express();
+var Mailchimp = require('mailchimp-api-v3')
+
+
+
+
+//Declaring PORT for deployment and testing
+var PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
+
+//Route to render home page
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/html/home.html");
+})
+
+//---------------------------MOVE TO CONFIG FILE IN PRODUCTION---------------------------
+let apiKey = "abdedd5d78f72187f2b2703945ae6c5e-us17"
+let postRoute = 'lists/7909d83c98/members'
+
+
+//Initiating new mailChimp object
+var mailchimp = new Mailchimp(apiKey);
+
+//function to test the pulling of data from the correct email list
+mailchimp.get({
+    path : '/lists/7909d83c98'
+  }, function (err, result) {
+    console.log(result.stats)
+  })
+
+
+//Send route for user submitted email data
+app.get("/send", function(req, res) {
+    console.log(req.query)
+    let data = req.query
+    console.log("data to be posted " + JSON.stringify(data))
+    mailchimp.post(postRoute, data
+    ).then(function(results) {
+        console.log("results " + JSON.stringify(results))
+    })
+       
+})
+
+
+//Initiating server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
